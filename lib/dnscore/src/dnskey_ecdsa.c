553b9d7d2a3c7cf56d1f02d73eb37cbd5a29e9df
Bernard Spil <brnrd@FreeBSD.org>
2017-02-25 12:32:24 +0100

    Fix build issues with LibreSSL
     - Check LIBRESSL_VERSION_NUMBER in addition to OpenSSL > 1.1
    
    LibreSSL forked from OpenSSL 1.0.1f and does not have all features
    from 1.1.0.

--- lib/dnscore/src/dnskey_ecdsa.c
+++ lib/dnscore/src/dnskey_ecdsa.c
@@ -79,7 +79,7 @@
 #define DNSKEY_ALGORITHM_ECDSAP256SHA256_NID NID_X9_62_prime256v1
 #define DNSKEY_ALGORITHM_ECDSAP384SHA384_NID NID_secp384r1
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 
 #define SSL_FIELD_GET(st_,f_) if(f_ != NULL) { *f_ = st_->f_; }
 #define SSL_FIELD_SET(st_,f_) if(f_ != NULL) { BN_free(st_->f_); st_->f_ = f_; }
